18012ca230fb21cb76487191ca915c0ba0a5f20e
Preeti Malakar <pmalakar@anl.gov>
2017-09-15 12:45:59 -0700

    Initial commit

---
+++ jobloc/parsejobnodes.py
@@ -0,0 +1,83 @@
+import os
+import sys
+
+def readGlobalConfiguration_(f, node_id, rank):
+
+  #f = open("theta.computenodes", "r")
+  cab_position, cab_row, cage, slot, cpu, processor_id = '', '', '', '', '', ''
+
+  while True:
+    line = f.readline()
+    if not line: break
+    words = line.split(',')
+    for word in words:
+      if 'cab_position' in word:
+       cab_position=word.split('=')[1]
+      elif 'cab_row' in word:
+       cab_row=word.split('=')[1]
+      elif 'cage=' in word:
+       cage=word.split('=')[1]
+      elif 'slot=' in word:
+       slot=word.split('=')[1]
+      elif 'cpu=' in word:
+       cpu=word.split('=')[1]
+      elif 'processor_id=' in word:
+       processor_id=word.split('=')[1]
+
+    if int(processor_id) == node_id:
+      print rank, processor_id, cab_position, cab_row, cage, slot, cpu
+      break
+
+#3572-3579,3727-3734
+def readAllocation_():
+  
+  f = open(nidstringfile, "r")
+  nidstring = f.readline()
+  f.close()
+
+	#parse
+  nodegroups = []
+  if nidstring.find(',') == -1:
+    nodegroups.append(nidstring)
+  else:
+    nodegroups = nidstring.split(',')
+
+  print 'JOB: Number of clusters in current allocation: ', len(nodegroups)
+  print
+
+  rank = -1
+  cluster = 0
+  for group in nodegroups:
+    #print group
+    group = group.rstrip('\n')
+    nodes = []
+    if group.find('-') == -1:
+      nodes.append(group)
+      nodes.append(group) #to generalize the below loop
+    else:
+      nodes = group.split('-')
+
+    f = open(nodeconfig, "r")
+    localrank = 0
+    for node in range(int(nodes[0]),int(nodes[1])+1):
+      rank = rank + 1
+      readGlobalConfiguration_(f, node, rank)
+      localrank = localrank + 1
+      #print node
+    f.close()
+    #print localrank
+
+    cluster = cluster + 1
+    centroidRank = (int(nodes[1]) - int(nodes[0]))/2 + int(nodes[0])
+    print 'JOB: centroid of cluster ', group, centroidRank
+    print
+    
+
+#config file
+nodeconfig = sys.argv[1]
+#current allocation
+nidstringfile = sys.argv[2] 
+
+#readGlobalConfiguration_()
+readAllocation_()
+
