73c7d98c9bfbf5179af9592e20ed88cae07344fd
Preeti Malakar <malakar.preeti@gmail.com>
2017-09-28 17:56:31 +0000

    adding info

---
+++ lnet/info.cxx
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define MAXBUFLEN 8192
+int rank, nprocs;
+int subsize, subrank, nidname;
+char pname[64];
+
+char * getinfo(char *fname) {
+
+  char *buf;
+  buf = (char *) malloc( 1024 * sizeof(char));
+
+  FILE *fp = fopen(fname,"r");
+  if (fp == NULL)
+    return NULL;
+
+  fgets(buf, MAXBUFLEN, fp);
+  fclose(fp);
+  return buf;
+
+}
+
+void getLNETroute() {
+
+   char *buf = new char[MAXBUFLEN];
+   char *routesconf = new char[MAXBUFLEN];
+   char *ip2nets = new char[MAXBUFLEN];
+   char *routes = new char[MAXBUFLEN];
+   char *stats = new char[MAXBUFLEN];
+
+   routesconf = getinfo ("/etc/lnet/routes.conf");
+   ip2nets = getinfo ("/etc/lnet/ip2nets.conf");
+   routes = getinfo ("/proc/sys/lnet/routes");
+   stats = getinfo ("/proc/sys/lnet/stats");
+
+   system ("grep current_conn /proc/fs/lustre/osc/snx11214-OST00*/import");
+   //buf = getinfo ("/proc/fs/lustre/osc/snx11214-OST00*/ost_conn_uuid"); 
+
+   if (subrank == 0) {
+      puts(routesconf);
+      puts(stats);
+   }  
+}
+
+int main(int argc, char* argv[]) {
+
+   int resultlen; 
+   MPI_Comm subcomm;
+   
+   MPI_Init (&argc, &argv);
+   MPI_Comm comm = MPI_COMM_WORLD;
+   MPI_Comm_size(comm, &nprocs);
+   MPI_Comm_rank(comm, &rank);
+
+   MPI_Get_processor_name(pname, &resultlen);
+   nidname = atoi(&pname[3]);
+
+   MPI_Comm_split(comm, nidname, rank, &subcomm); 
+   MPI_Comm_size(subcomm, &subsize);
+   MPI_Comm_rank(subcomm, &subrank);
+   printf("pname of %d %s : nidname %d: %d of %d\n", rank, pname, nidname, subrank, subsize);
+  
+   getLNETroute();  
+   MPI_Finalize();
+
+   return 0;
+}
+
---
+++ lnet/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -l
+#COBALT -A Performance
+#COBALT -n 4
+
+#SBATCH -p debug
+#SBATCH -t 00:10:00
+#SBATCH -N 2
+#SBATCH -J my_job
+#SBATCH -C knl,quad,cache    ####haswell
+##SBATCH -o my_job.o%j
+
+
+export MPICH_MPIIO_HINTS_DISPLAY=1
+export MPICH_MPIIO_AGGREGATOR_PLACEMENT_DISPLAY=1 
+export MPICH_MPIIO_STATS=1
+export MPICH_MPIIO_TIMERS=1
+
+echo "***"
+
+EXE="./info"
+
+if [[ "$HOST" == *"cori"* ]]; then
+#cori
+echo "cori"
+grep current_conn /proc/fs/lustre/osc/snx11168-OST00*/import
+cat /proc/fs/lustre/osc/snx11168-OST00*/ost_conn_uuid 
+nodes=$SLURM_JOB_NUM_NODES
+echo $SLURM_NODELIST
+srun -n 4 -N 2 ./status.knl
+fi
+
+if [[ "$HOST" == *"theta"* ]]; then
+#theta
+
+echo "theta"
+ppn=2
+RANKS=$((${COBALT_PARTSIZE}*$ppn))
+
+APRUNPARAMS=" -n ${RANKS} -N ${ppn} -d 1 -j 1 -r 1 " #--attrs mcdram=cache:numa=quad "
+ENVVARS=""
+
+#grep current_conn /proc/fs/lustre/osc/snx11214-OST00*/import
+#cat /proc/fs/lustre/osc/snx11214-OST00*/ost_conn_uuid 
+
+aprun ${ENVVARS} ${APRUNPARAMS} ${EXE}
+
+fi
+
+echo "***"
