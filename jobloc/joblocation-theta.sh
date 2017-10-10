5376673fde1b2a844e4226d3e39bc8ac63bfb5ec
Preeti Malakar <malakar.preeti@gmail.com>
2017-10-10 11:21:34 +0000

    updating

---
+++ jobloc/joblocation-theta.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#COBALT -t 30
+####COBALT -n 1
+#COBALT -A Performance
+
+EXE=./location.x
+
+echo "***"
+
+if [ $HOST == *"cori"* ] ; then 
+#cori
+echo "cori"
+grep current_conn /proc/fs/lustre/osc/snx11168-OST00*/import
+cat /proc/fs/lustre/osc/snx11168-OST00*/ost_conn_uuid 
+nodes=$SLURM_JOB_NUM_NODES
+echo $SLURM_NODELIST
+srun -n 4 -N 2 ./status.knl
+fi
+
+if [[ "$HOST" == *"theta"* ]]; then
+#theta
+echo "theta"
+ppn=4
+RANKS=$((${COBALT_PARTSIZE}*$ppn))
+
+APRUNPARAMS=" -n ${RANKS} -N ${ppn} -d 1 -j 1 -r 1 " #--attrs mcdram=cache:numa=quad "
+ENVVARS=""
+
+grep current_conn /proc/fs/lustre/osc/snx11214-OST00*/import
+cat /proc/fs/lustre/osc/snx11214-OST00*/ost_conn_uuid 
+
+aprun ${ENVVARS} ${APRUNPARAMS} ${EXE}
+
+fi
+echo "***"
--- jobloc/location.c
+++ jobloc/location.c
@@ -6,21 +6,27 @@
 
 #include "xctopo.h"
 
-int main(int argc, char * argv[])
+int rank, size, subrank;
+
+int getNodeInfo ()
 {
+
   int rc;
-  int rank, size;
+  int nid, subsize;
+  MPI_Comm subcomm;
+  char pname[64];
+  //FILE *nidfile = fopen("/proc/cray_xt/nid", "r");
+  //if (nidfile!=NULL) 
+  //  fscanf(nidfile, "%d", &nid);
+  //fclose(nidfile);
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  int nid;
-  FILE *nidfile = fopen("/proc/cray_xt/nid", "r");
-  if (nidfile!=NULL) 
-    fscanf(nidfile, "%d", &nid);
-  fclose(nidfile);
-
+  MPI_Get_processor_name(pname, &rc);
+  nid = atoi(&pname[3]);
+  MPI_Comm_split(MPI_COMM_WORLD, nid, rank, &subcomm); 
+  MPI_Comm_size(subcomm, &subsize);
+  MPI_Comm_rank(subcomm, &subrank);
+//  printf("pname of %d %s : nidname %d: %d of %d\n", rank, pname, nidname, subrank, subsize);
+  
   xctopo_t topo;
   rc = xctopo_get_mycoords(&topo);
 
@@ -30,7 +36,19 @@ int main(int argc, char * argv[])
   int slot  = topo.slot;
   int anode = topo.anode;
 
-  printf("%d %d %d %d %d %d %d \n", rank, nid, col, row, cage, slot, anode);
+  printf("%d (%d) %d %d %d %d %d %d\n", rank, subrank, nid, col, row, cage, slot, anode);
+
+  return 0;
+
+}
+
+int main(int argc, char * argv[])
+{
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  getNodeInfo();
 
   MPI_Finalize();
 
