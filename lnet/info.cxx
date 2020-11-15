4deb9ec04e904289d161b1b3d0d44b1efd112ff9
Preeti Malakar <preeti.malakar@gmail.com>
2020-11-15 05:53:06 -0800

    resolving namespace issue

--- lnet/info.cxx
+++ lnet/info.cxx
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int rank, nprocs;
+int myrank, nprocs;
 int subsize, subrank, nidname;
 char pname[64];
 
@@ -36,9 +36,9 @@ void getLNETroute() {
 
    if (subrank == 0) {
      routesconf = getinfo ("/etc/lnet/routes.conf"); 
-     printf ("%d %d routesconf %s\n", rank, nprocs, routesconf);
+     printf ("%d %d routesconf %s\n", myrank, nprocs, routesconf);
      ip2nets = getinfo ("/etc/lnet/ip2nets.conf");
-     printf ("%d %d ip2netsconf %s\n", rank, nprocs, ip2nets);
+     printf ("%d %d ip2netsconf %s\n", myrank, nprocs, ip2nets);
    //  system ("cat /proc/sys/lnet/routes");
    //  system ("cat /proc/sys/lnet/stats");
    } 
@@ -53,15 +53,15 @@ int main(int argc, char* argv[]) {
    MPI_Init (&argc, &argv);
    MPI_Comm comm = MPI_COMM_WORLD;
    MPI_Comm_size(comm, &nprocs);
-   MPI_Comm_rank(comm, &rank);
+   MPI_Comm_rank(comm, &myrank);
 
    MPI_Get_processor_name(pname, &resultlen);
    nidname = atoi(&pname[3]); 
 
-   MPI_Comm_split(comm, nidname, rank, &subcomm); 
+   MPI_Comm_split(comm, nidname, myrank, &subcomm); 
    MPI_Comm_size(subcomm, &subsize);
    MPI_Comm_rank(subcomm, &subrank);
-//   printf("pname of %d %s : nidname %d: %d of %d\n", rank, pname, nidname, subrank, subsize);
+//   printf("pname of %d %s : nidname %d: %d of %d\n", myrank, pname, nidname, subrank, subsize);
 
    getLNETroute();  
    MPI_Finalize();
